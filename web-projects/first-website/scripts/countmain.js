1bbfa6c6d5956068a91ae95b0cca7be448c59442
OishiKaisei <kaisei.oishi@tsukasa-ind.co.jp>
2025-04-24 16:13:27 +0900

    Update countmain.js

--- web-projects/first-website/scripts/countmain.js
+++ web-projects/first-website/scripts/countmain.js
@@ -1,4 +1,38 @@
-const myImage = document.querySelector("img");
+function count() {
+    //三角形
+	const up = document.querySelector("#countup-id-top");
+	const down = document.querySelector("#countdown-id-top");
+    const upbottom = document.querySelector("#countup-id-bottom");
+    const downbottom = document.querySelector("#countdown-id-bottom");
+    //数字
+	const top = document.querySelector("#top-id");
+	const bottom = document.querySelector("#bottom-id");
+    //バツボタン
+    const vatu = document.querySelector("#vatubottom");
+	up.onclick = () => {
+		const topCount = Number(top.textContent);
+		top.textContent = topCount + 1;
+	};
 
-myImage.onclick = () => {
+	down.onclick = () => {
+		const bottomCount = Number(top.textContent);
+		top.textContent = bottomCount - 1;
+	};
+
+    upbottom.onclick = () => {
+		const topCountbotton = Number(bottom.textContent);
+		bottom.textContent = topCountbotton + 1;
+	};
+
+	downbottom.onclick = () => {
+		const bottomCountbottom = Number(bottom.textContent);
+		bottom.textContent = bottomCountbottom - 1;
+	};
+    vatu.onclick = () =>{
+        top.textContent = 20;
+        bottom.textContent = 20;
+    }
 }
+
+count();
+
