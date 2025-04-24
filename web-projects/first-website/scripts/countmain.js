f357f93e426e250eafeeee5a4e195c69b0230a70
OishiKaisei <kaisei.oishi@tsukasa-ind.co.jp>
2025-04-25 08:52:46 +0900

    Update countmain.js

--- web-projects/first-website/scripts/countmain.js
+++ web-projects/first-website/scripts/countmain.js
@@ -1,27 +1,37 @@
-html {
-  font-size: 15px; 
-  text-align: center;
-  font-family:
-  "Kosugi Maru", sans-serif;
-  user-select: none;/*選択無効*/
+function count() {
+    //三角形
+	const up = document.querySelector("#countup-id-top");
+	const down = document.querySelector("#countdown-id-top");
+    const upbottom = document.querySelector("#countup-id-bottom");
+    const downbottom = document.querySelector("#countdown-id-bottom");
+    //数字
+	const top = document.querySelector("#top-id");
+	const bottom = document.querySelector("#bottom-id");
+    //バツボタン
+    const button = document.querySelector("#close-button");
+	up.onclick = () => {
+		const topCount = Number(top.textContent);
+		top.textContent = topCount + 1;
+	};
+
+	down.onclick = () => {
+		const bottomCount = Number(top.textContent);
+		top.textContent = bottomCount - 1;
+	};
+
+    upbottom.onclick = () => {
+		const topCountbotton = Number(bottom.textContent);
+		bottom.textContent = topCountbotton + 1;
+	};
+
+	downbottom.onclick = () => {
+		const bottomCountbottom = Number(bottom.textContent);
+		bottom.textContent = bottomCountbottom - 1;
+	};
+    button.onclick = () =>{
+        top.textContent = 20;
+        bottom.textContent = 20;
+    }
 }
 
-#top-id{
-  font-size: 200px;
-  margin: 80px;
-}
-
-#bottom-id{
-  font-size: 200px;
-  margin: 80px;
-}
-
-
-#black{
-  height: 40px;
-  background-color: #000000;
-   font-size: 40px;
-color: white;
-display: flex;
-justify-content: flex-end; 
-}
+count();
