fa70478e3e51d2a354a60df9200b77bed20beb15
OishiKaisei <kaisei.oishi@tsukasa-ind.co.jp>
2025-04-25 10:09:16 +0900

    Update countmain.js

--- web-projects/first-website/scripts/countmain.js
+++ web-projects/first-website/scripts/countmain.js
@@ -1,14 +1,14 @@
 function count() {
     //三角形
-	const up = document.querySelector("#countup-id-top");
-	const down = document.querySelector("#countdown-id-top");
-    const upbottom = document.querySelector("#countup-id-bottom");
-    const downbottom = document.querySelector("#countdown-id-bottom");
+	const up = document.querySelector("#countup_id_top");
+	const down = document.querySelector("#countdown_id_top");
+    const upBottom = document.querySelector("#countup_id_bottom");
+    const downBottom = document.querySelector("#countdown_id_bottom");
     //数字
-	const top = document.querySelector("#top-id");
-	const bottom = document.querySelector("#bottom-id");
+	const top = document.querySelector("#top_id");
+	const bottom = document.querySelector("#bottom_id");
     //バツボタン
-    const button = document.querySelector("#close-button");
+    const button = document.querySelector("#close_button");
 	up.onclick = () => {
 		const topCount = Number(top.textContent);
 		top.textContent = topCount + 1;
@@ -19,14 +19,14 @@ function count() {
 		top.textContent = bottomCount - 1;
 	};
 
-    upbottom.onclick = () => {
-		const topCountbotton = Number(bottom.textContent);
-		bottom.textContent = topCountbotton + 1;
+    upBottom.onclick = () => {
+		const topCountButton = Number(bottom.textContent);
+		bottom.textContent = topCountButton + 1;
 	};
 
-	downbottom.onclick = () => {
-		const bottomCountbottom = Number(bottom.textContent);
-		bottom.textContent = bottomCountbottom - 1;
+	downBottom.onclick = () => {
+		const bottomCountBottom = Number(bottom.textContent);
+		bottom.textContent = bottomCountBottom - 1;
 	};
     button.onclick = () =>{
         top.textContent = 20;
