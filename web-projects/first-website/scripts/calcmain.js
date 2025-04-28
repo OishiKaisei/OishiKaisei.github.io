8ae85622dd54b68c74b11ee5637f1cc1f6946fdd
OishiKaisei <kaisei.oishi@tsukasa-ind.co.jp>
2025-04-28 16:53:22 +0900

    Update calcmain.js

--- web-projects/first-website/scripts/calcmain.js
+++ web-projects/first-website/scripts/calcmain.js
@@ -1,38 +1,36 @@
 function calc() {
-const number = document.querySelector("#number_display");//数字
-const plus = document.querySelector("#plus_button");//足し算足し算
-const minus = document.querySelector("#minus_button");//引き算
-const times = document.querySelector("#times_button");//掛け算
-const div = document.querySelector("#div_button");//割り算
-const reset = document.querySelector("#reset_button");//リセットボタン
+const calcResult = document.querySelector("#number_display");//数字
+const plusButton = document.querySelector("#plus_button");//足し算足し算
+const minusButton = document.querySelector("#minus_button");//引き算
+const timesButton = document.querySelector("#times_button");//掛け算
+const divButton = document.querySelector("#div_button");//割り算
+const resetButton = document.querySelector("#reset_button");//リセットボタン
 
 
-plus.onclick = () => {
-  const numberDisplay = Number(number.textContent);
-  number.textContent =  numberDisplay + 1;
+plusButton.onclick = () => {
+  const calcResultDisplay = Number(calcResult.textContent);
+  calcResult.textContent =  calcResultDisplay + 1;
 }
 
-minus.onclick = () => {
-  const numberDisplay = Number(number.textContent);
-  number.textContent =  numberDisplay - 1;
+minusButton.onclick = () => {
+  const calcResultDisplay = Number(calcResult.textContent);
+  calcResult.textContent =  calcResultDisplay - 1;
 }
 
-times.onclick = () => {
-  const numberDisplay = Number(number.textContent);
-  number.textContent =  numberDisplay * 2;
+timesButton.onclick = () => {
+  const calcResultDisplay = Number(calcResult.textContent);
+  calcResult.textContent =  calcResultDisplay * 2;
 }
 
-div.onclick = () => {
-  const numberDisplay = Number(number.textContent);
-  number.textContent =  numberDisplay / 2;
+divButton.onclick = () => {
+  const calcResultDisplay = Number(calcResult.textContent);
+  calcResult.textContent =  calcResultDisplay / 2;
 }
 
-reset.onclick = () => {
-  const numberDisplay = Number(number.textContent);
-  number.textContent =  100;
+resetButton.onclick = () => {
+  const calcResultDisplay = Number(calcResult.textContent);
+  calcResult.textContent =  100;
 }
-
-
 
 }
 calc()
